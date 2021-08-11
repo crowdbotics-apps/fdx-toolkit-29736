eed3152d3dcd8669cf76ad76e5c8a952b497dec8
Crowdbotics <team@crowdbotics.com>
2021-08-11 23:31:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fdx_toolkit_29736Identifier",
+  url: "https://fdx_toolkit_29736Identifier.botics.co",
+  api: "https://fdx_toolkit_29736Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
